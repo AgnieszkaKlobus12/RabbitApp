d94979054a10cc9962671e57d554ffa54be85362
Agnieszka <aga.hagneia@gmail.com>
2024-06-21 21:32:47 +0200

    edit vaccine

--- app/src/main/java/com/example/rabbitapp/ui/vaccines/VaccineEditFragment.kt
+++ app/src/main/java/com/example/rabbitapp/ui/vaccines/VaccineEditFragment.kt
@@ -7,12 +7,16 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.navArgs
 import com.example.rabbitapp.R
 import com.example.rabbitapp.databinding.FragmentVaccineEditBinding
 import com.example.rabbitapp.model.entities.Vaccine
 import com.example.rabbitapp.ui.mainTab.MainListViewModel
 
 class VaccineEditFragment : Fragment() {
+
+    private val args: VaccineEditFragmentArgs by navArgs()
+    private var vaccine: Vaccine? = null
 
     private var _binding: FragmentVaccineEditBinding? = null
     private val binding get() = _binding!!
@@ -24,15 +28,22 @@ class VaccineEditFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentVaccineEditBinding.inflate(inflater, container, false)
+        vaccine = viewModel.getVaccine(args.vaccineId)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        vaccine?.let {
+            binding.fragmentVaccineEditVaccineName.setText(it.name)
+            binding.fragmentVaccineEditVaccineDescription.setText(it.description)
+        }
+
         binding.addVaccineSaveButton.setOnClickListener {
             viewModel.save(
                 Vaccine(
-                    0,
+                    vaccine?.id ?: 0,
                     binding.fragmentVaccineEditVaccineName.text.toString(),
                     binding.fragmentVaccineEditVaccineDescription.text.toString()
                 )
