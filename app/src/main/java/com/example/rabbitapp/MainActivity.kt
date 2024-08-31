b545611b9b44b9a84ed502368cbb80f470388737
Agnieszka <aga.hagneia@gmail.com>
2024-08-31 20:08:22 +0200

    google drive synchronization

--- app/src/main/java/com/example/rabbitapp/MainActivity.kt
+++ app/src/main/java/com/example/rabbitapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.rabbitapp
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -9,7 +10,21 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.rabbitapp.databinding.ActivityMainBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.common.Scopes
 import com.google.android.material.navigation.NavigationView
+import com.google.api.client.extensions.android.http.AndroidHttp
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential
+import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException
+import com.google.api.client.http.FileContent
+import com.google.api.client.json.gson.GsonFactory
+import com.google.api.services.drive.Drive
+import com.google.api.services.drive.DriveScopes
+import com.google.api.services.drive.model.File
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +32,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var appBarConfiguration: AppBarConfiguration
 
+    @OptIn(DelicateCoroutinesApi::class)
     @SuppressLint("RestrictedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,10 +55,78 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        GlobalScope.launch(Dispatchers.IO) {
+            upload()
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
+
+    private fun upload() {
+        val googleSignInAccount = GoogleSignIn.getLastSignedInAccount(this)
+        val credential = GoogleAccountCredential.usingOAuth2(
+            this,
+            setOf(Scopes.DRIVE_FILE, DriveScopes.DRIVE_APPDATA)
+        )
+        credential.setSelectedAccount(googleSignInAccount!!.account)
+
+        val googleDriveService = Drive.Builder(
+            AndroidHttp.newCompatibleTransport(),
+            GsonFactory(),
+            credential
+        )
+            .setApplicationName(getString(R.string.app_name))
+            .build()
+
+        val dbPath = applicationContext.getDatabasePath("app_database").path
+        val dbPathShm =
+            applicationContext.getDatabasePath("app_database").parent?.plus("/app_database-shm")
+        val dbPathWal =
+            applicationContext.getDatabasePath("app_database").parent?.plus("/app_database-wal")
+
+        val storageFile = File().apply {
+            parents = listOf("appDataFolder")
+            name = "app_database"
+        }
+
+        val storageFileShm = File().apply {
+            parents = listOf("appDataFolder")
+            name = "app_database-shm"
+        }
+
+        val storageFileWal = File().apply {
+            parents = listOf("appDataFolder")
+            name = "app_database-wal"
+        }
+
+        val filePath = java.io.File(dbPath)
+        val filePathShm = dbPathShm?.let { java.io.File(it) }
+        val filePathWal = dbPathWal?.let { java.io.File(it) }
+        val mediaContent = FileContent("", filePath)
+        val mediaContentShm = FileContent("", filePathShm)
+        val mediaContentWal = FileContent("", filePathWal)
+
+        try {
+            val file = googleDriveService.files().create(storageFile, mediaContent).execute()
+            Log.d("Google Drive", "Filename: ${file.name} File ID: ${file.id}")
+
+            val fileShm =
+                googleDriveService.files().create(storageFileShm, mediaContentShm).execute()
+            Log.d("Google Drive", "Filename: ${fileShm.name} File ID: ${fileShm.id}")
+
+            val fileWal =
+                googleDriveService.files().create(storageFileWal, mediaContentWal).execute()
+            Log.d("Google Drive", "Filename: ${fileWal.name} File ID: ${fileWal.id}")
+        } catch (e: UserRecoverableAuthIOException) {
+            startActivityForResult(e.intent, 1)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/rabbitapp/ui/settings/login/SettingsFragment.kt
+++ app/src/main/java/com/example/rabbitapp/ui/settings/login/SettingsFragment.kt
@@ -1,11 +1,17 @@
 package com.example.rabbitapp.ui.settings.login
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.rabbitapp.R
 import com.example.rabbitapp.databinding.FragmentSettingsBinding
+import com.example.rabbitapp.ui.login.LoginActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 
 class SettingsFragment : Fragment() {
 
@@ -19,6 +25,25 @@ class SettingsFragment : Fragment() {
     ): View {
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.logOutButton.setOnClickListener {
+            val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(resources.getString(R.string.google_server_client_id))
+                .requestEmail()
+                .build()
+            val mGoogleSignInClient =
+                GoogleSignIn.getClient(requireActivity().applicationContext, gso)
+            val account = GoogleSignIn.getLastSignedInAccount(requireActivity().applicationContext)
+            Log.i("google account", account.toString())
+            if (account != null)
+                mGoogleSignInClient.signOut()
+            val intent = Intent(context, LoginActivity::class.java)
+            requireActivity().startActivity(intent)
+        }
     }
 
 }
